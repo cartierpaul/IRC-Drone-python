28d27e79f79024a137214e18ea8ca808d56ad0f3
Cartier P <140940449+cartierpaul@users.noreply.github.com>
2023-08-04 10:45:21 +0200

    Create xml.parsers.expat.py

---
+++ xml.parsers.expat.py
@@ -0,0 +1,4 @@
+# Embedded file name: /home/boris/undernetbot/build/run/out00-PYZ.pyz/xml.parsers.expat
+"""Interface to the Expat non-validating XML parser."""
+__version__ = '$Revision: 17640 $'
+from pyexpat import *
