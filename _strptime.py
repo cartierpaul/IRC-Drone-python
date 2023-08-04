abbabfbd34b412cc278646feb2d9eac602e77226
Cartier P <140940449+cartierpaul@users.noreply.github.com>
2023-08-04 10:38:37 +0200

    Create _strptime.py

---
+++ _strptime.py
@@ -0,0 +1,13 @@
+# Embedded file name: /home/boris/undernetbot/build/run/out00-PYZ.pyz/_strptime
+"""Strptime-related classes and functions.
+
+CLASSES:
+    LocaleTime -- Discovers and stores locale-specific time information
+    TimeRE -- Creates regexes for pattern matching a string of text containing
+                time information
+
+FUNCTIONS:
+    _getlang -- Figure out what language is being used for the locale
+    strptime -- Calculates the time struct represented by the passed-in string
+
+"""
