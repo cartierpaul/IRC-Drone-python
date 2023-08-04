70e7d4e0126e341fc75486e9025648daeb1a45dd
Cartier P <140940449+cartierpaul@users.noreply.github.com>
2023-08-04 10:45:23 +0200

    Create xml.parsers.py

---
+++ xml.parsers.py
@@ -0,0 +1,10 @@
+# Embedded file name: /home/boris/undernetbot/build/run/out00-PYZ.pyz/xml.parsers
+"""Python interfaces to XML parsers.
+
+This package contains one module:
+
+expat -- Python wrapper for James Clark's Expat parser, with namespace
+         support.
+
+"""
+pass
