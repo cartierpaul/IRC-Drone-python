fcd7e7adbfef5b9f1f94213046bce1719718f7f1
Cartier P <140940449+cartierpaul@users.noreply.github.com>
2023-08-04 10:34:37 +0200

    Create Scripts.Vars.py

---
+++ Scripts.Vars.py
@@ -0,0 +1,17 @@
+DEBUG = False
+DA = 'HASH-HASH-HASH-HASH-HASH-HASH-PAPANASH=='
+PG = 'xtfi4sfj59mVlN_U'
+VERSdark = 'BanditBot'
+CODER = 'v2.7, BY -@CARTIER- (Muie Cristi)'
+CONFIG_FILE = 'cfg'
+PID_FILE = 'pid'
+ID_FILE = 'id'
+DEFCMDCHAR = '@'
+DEK = 'MUIECRISTI'
+MAXNICKNAMES = 12
+MAXCHANNELS = 15
+MAXSERVERS = 15
+MAXADMINS = 10
+MAXBUFFERSIZE = 1024
+ISONINTERVAL = 2.4
+NAMES = 'love gov acid nasa wolf soul kill android apollo demon epic best bomba burn godzilla berlin bank ban kid mad sad bad god love lord idle\naway Full fly sex sexy ccpower error gov nasa fbi pong ping master kiss ice mad bad knight king flyer bear animal thebeast batman spider sad star hero voice natural null hades soul single sound music\nmovie reader evil ghost devil whois fear hate fate paul cartier nobody kill\nkiller dark\n'
